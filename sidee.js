4e2e86ede5b3bee165ddd126b67d79952897e873
G-Silvaa <93489073+G-Silvaa@users.noreply.github.com>
2023-03-27 11:52:55 -0300

    Add files via upload

--- sidee.js
+++ sidee.js
@@ -3,10 +3,6 @@
 const btnMobile = document.querySelector('.btn_icon_header')
 var navigationHeader = document.getElementById('navigation-header');
 
-console.log(navigationHeader)
-
-console.log(btnMobile)
-
 function toggleMenu() {
     navigationHeader.classList.toggle('active');
     
